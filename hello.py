b07deaa4dfc84fbc42268e439fce3d0861c422fb
fanwenl <fanwenl_g@126.com>
2018-11-11 16:18:09 +0800

    add hello function (reverted from commit 587b88050c4196916eefd2bdba6cc3ef1c0bdb05)

--- hello.py
+++ hello.py
@@ -1,13 +1,3 @@
 # -*-coding:utf-8 -*-
 def hello_world():
     print('hello world!')
-
-def printf(name):
-    print('hello,' + name)
-
-def main():
-    hello_world()
-    printf('git')
-
-if __name__ == '__main__':
-    main()
