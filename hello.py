27f147a0c7dd788586fe3b651b80b170da80ff05
fanwenl <fanwenl_g@126.com>
2018-11-11 15:34:24 +0800

    add printf git

--- hello.py
+++ hello.py
@@ -2,8 +2,13 @@
 def hello_world():
     print('hello world!')
 
+def printf(name):
+    print('hello,' + name)
+
 def main():
     hello_world()
+    printf('git')
+
 
 if __name__ == '__main__':
-    main();
+    main()
