f513e5c3faf1d5ce5bb34645a5309136221697b5
fanwenl <fanwenl_g@126.com>
2018-11-11 16:33:57 +0800

    swv

--- hello.py
+++ hello.py
@@ -1,3 +1,22 @@
 # -*-coding:utf-8 -*-
 def hello_world():
     print('hello world!')
+
+
+込込込限込gh
+
+
+hahajhfak
+
+
+agjaklgjaljhal、
+
+
+ajgkajklgjakgjakhgakh'
+
+
+
+def 
+
+(self, parameter_list):
+    pass
